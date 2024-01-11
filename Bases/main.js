ea70b23b6f7d133d16a09bd311be3d50825eb652
David Zapata <dzapata@saki.com.co>
2024-01-11 13:10:17 -0500

    se completo el ejercicio de interfaces y se comenzo la clase de namespaces

--- Bases/main.js
+++ Bases/main.js
@@ -1,79 +1,14 @@
 "use strict";
-(() => {
-    class Mutante {
-        constructor(name, realName) {
-            this.name = name;
-            this.realName = realName;
-        }
-    }
-    class Xmen extends Mutante {
-        salvarMundo() {
-            console.log('Mundo Salvado!');
-        }
-    }
-    class Villano extends Mutante {
-        conquistarMundo() {
-            console.warn('El mundo esta siendo conquistado!');
-        }
-    }
-    const wolverine = new Xmen('Wolverine', 'Logan');
-    const magneto = new Villano('Magneto', 'Megan');
-    const printName = (character) => {
-        console.log(character.name);
+var Validations;
+(function (Validations) {
+    Validations.validateTxt = (text) => {
+        return (text.length > 3) ? true : false;
     };
-    printName(wolverine);
-    printName(magneto);
-})();
-(() => {
-    class Avenger {
-        static getClassName() {
-            return this.name;
-        }
-        constructor(name, team, realName) {
-            this.name = name;
-            this.team = team;
-            this.realName = realName;
-        }
-        bio() {
-            return `${this.name} (${this.team})`;
-        }
-    }
-    Avenger.avgAge = 35;
-    const antMan = new Avenger('Antman', 'Capi', 'Scorr');
-})();
-(() => {
-    class Avenger {
-        constructor(name, realName) {
-            this.name = name;
-            this.realName = realName;
-        }
-        getFullName() {
-            return `${this.name} ${this.realName}`;
-        }
-    }
-    class Xmen extends Avenger {
-        constructor(name, realName, isMutant = true, _debilidad = false, _afinidad = 'michis') {
-            super(name, realName);
-            this.isMutant = isMutant;
-            this._debilidad = _debilidad;
-            this._afinidad = _afinidad;
-        }
-        get debilidad() {
-            return this._debilidad;
-        }
-        set debilidad(d) {
-            this._debilidad = d;
-        }
-        get afinidad() {
-            return this._afinidad;
-        }
-        set afinidad(a) {
-            this._afinidad = a;
-        }
-        getRealNameFromXmen() {
-            return this.getFullName();
-        }
-    }
-    const wolverine = new Xmen('Wolverine', 'Logan');
-})();
+    Validations.validateDate = (myDate) => {
+        return (isNaN(myDate.valueOf()));
+    };
+})(Validations || (Validations = {}));
+console.log(Validations);
+console.log(Validations.validateTxt('fer'));
+console.log(Validations.validateTxt('fernaflo'));
 //# sourceMappingURL=main.js.map
