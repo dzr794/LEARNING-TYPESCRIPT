ad7a59c2b094fd058c81984146ec76aeccf49e08
David Zapata <dzapata@saki.com.co>
2024-01-15 14:09:53 -0500

    fin ejemplo, bloquear extension

--- ts-node/src/decorators/pokemon-class.ts
+++ ts-node/src/decorators/pokemon-class.ts
@@ -3,7 +3,7 @@ function printToConsole( constructor: Function ){
   console.log(constructor); 
 }
 
-// definicion de decorador como funcion de flecha
+// definicion de decorador como funcion de flecha con condicionales para cambiar la funcionalidad del decorador
 const printToConsoleConditional = ( print:boolean = false ):Function => {
   if (print) {
     return printToConsole;
@@ -12,7 +12,13 @@ const printToConsoleConditional = ( print:boolean = false ):Function => {
   }
 }
 
+const bloquearPrototipo = (constructor:Function) => {
+  Object.seal(constructor);
+  Object.seal(constructor.prototype);
+}
+
 // El decorador se ejecuta solo UNA VEZ (al momento de la transpilación del codigo)
+@bloquearPrototipo
 @printToConsoleConditional(true)
 export class Pokemon{
   public publicAPI: string = 'https://pokeapi.co';
