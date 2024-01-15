56ed0749d31caa789fc6bf2b5b8ba93bb7d74fd7
David Zapata <dzapata@saki.com.co>
2024-01-15 12:51:49 -0500

    fin seccion de generics, comienza seccion de decoradores

--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,6 +1,6 @@
 import { getPokemon } from "./generics/get-pokemon";
 
  getPokemon(1)
-  .then( resp => console.log(resp) )
+  .then( pokemon => console.log(pokemon) )
   .catch( error => console.error(error) )
   .finally( () => console.log('Fin de getPokemon') );
--- ts-node/src/interfaces/index.ts
+++ ts-node/src/interfaces/index.ts
@@ -1,2 +1,5 @@
+//* Son buenas practicas tener este documento ordenando las lineas por orden ascendente
 export { Hero } from './hero';
+export { Pokemon } from './pokemon-con-quicktype';
+//// export { MiniPokemon } from './pokemon';
 export { Villano } from './villian';
