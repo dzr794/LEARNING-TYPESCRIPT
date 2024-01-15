345a276516708ea30fb51e10ba6af71291d44ea5
David Zapata <dzapata@saki.com.co>
2024-01-15 14:00:27 -0500

    fin decoradores de fabrica

--- ts-node/src/decorators/pokemon-class.ts
+++ ts-node/src/decorators/pokemon-class.ts
@@ -1,10 +1,19 @@
+// definición del decorador
 function printToConsole( constructor: Function ){
-  console.log(constructor);
-  
+  console.log(constructor); 
+}
+
+// definicion de decorador como funcion de flecha
+const printToConsoleConditional = ( print:boolean = false ):Function => {
+  if (print) {
+    return printToConsole;
+  }else{
+    return () => {}
+  }
 }
 
 // El decorador se ejecuta solo UNA VEZ (al momento de la transpilación del codigo)
-@printToConsole
+@printToConsoleConditional(true)
 export class Pokemon{
   public publicAPI: string = 'https://pokeapi.co';
 
