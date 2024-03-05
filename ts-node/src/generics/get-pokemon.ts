465ba7aadb150fb04e861be17cb97ed1956e11c8
David Zapata <dzr794@gmail.com>
2024-03-05 09:18:50 -0500

    removing vscode from repo

--- ts-node/src/generics/get-pokemon.ts
+++ ts-node/src/generics/get-pokemon.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { Pokemon } from '../interfaces';
 
+// Aqui se utiliza axios para las peticiones REST
 export const getPokemon = async( pokeId:number ): Promise<Pokemon> => {
   const {data} = await axios.get<Pokemon>(`https://pokeapi.co/api/v2/pokemon/${pokeId}`);
 
